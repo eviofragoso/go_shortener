9e9c0753fe7f98a1a2eacfe4a7e683b838d26d81
Evio Fragoso <evio.junior@spezi.com.br>
2021-02-03 08:50:47 -0300

    Added base functionality

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/eviofragoso/go_shortener
 
 go 1.14
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func hash(s string) uint32 {
@@ -27,9 +29,20 @@ func token(original string) string {
 	return token
 }
 
-func retrieveToken(original string) string {
-	// Open our jsonFile
-	database, err := os.Open("database.json")
+func url(token string) string {
+	result := readFileData("database.json")
+
+	for k, v := range result {
+		if v.(string) == token {
+			return k
+		}
+	}
+
+	return ""
+}
+
+func readFileData(fileName string) map[string]interface{} {
+	database, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -39,6 +52,13 @@ func retrieveToken(original string) string {
 	var result map[string]interface{}
 	json.Unmarshal([]byte(byteValue), &result)
 
+	return result
+}
+
+func retrieveToken(original string) string {
+	// Open our jsonFile
+	result := readFileData("database.json")
+
 	if result[original] != nil {
 		return fmt.Sprintf("%v", result[original])
 	}
@@ -47,20 +67,32 @@ func retrieveToken(original string) string {
 }
 
 func createToken(original string) string {
+	result := readFileData("database.json")
+	re := regexp.MustCompile(`http(?:s)?://(?:\w+\.)?\w+\.\w+(?:\.\w+)?`)
+
+	if re.MatchString(original) == false {
+		return ""
+	}
 
 	urlHash := hash(original)
+	result[original] = fmt.Sprintf("%v", urlHash)
+	//
 	// Open :our jsonFile
-	database, err := os.Open("database.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer database.Close()
+	file, _ := json.MarshalIndent(result, "", " ")
+	_ = ioutil.WriteFile("database.json", file, 0644)
 
+	return fmt.Sprintf("%v", urlHash)
 }
 
 func shortenedURL(original string) string {
-	domain := os.Getenv("DOMAIN")
+	domain := os.Getenv("SHORTENER_DOMAIN")
+	token := token(original)
 
+	if token == "" {
+		return token
+	}
+
+	return fmt.Sprintf("%v/%v", domain, token)
 }
 
 // GetShortenedURL retrieve shortened url based on original
@@ -68,13 +100,38 @@ func serveShortenedURL(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	original := params["url"]
 
+	if original == "" {
+		json.NewEncoder(w).Encode("")
+
+		return
+	}
+
 	json.NewEncoder(w).Encode(shortenedURL(original))
 }
 
+func redirectToURL(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	hash := params["hash"]
+	url := url(hash)
+
+	if url != "" {
+		http.Redirect(w, r, url, http.StatusSeeOther)
+	}
+}
+
 func main() {
+	if os.Getenv("GO_ENV") == "" || os.Getenv("GO_ENV") == "development" {
+		err := godotenv.Load()
+
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", serveShortenedURL).Methods("GET")
+	router.HandleFunc("/shortener", serveShortenedURL).Queries("url", "{url}").Methods("GET")
+	router.HandleFunc("/{hash}", redirectToURL).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
